2fd381b610e5b94a5d72684682eaec17cf3e29a8
Yong <yong@runllama.ai>
2026-08-11 12:29:33 -0700

    seal

--- organizationuser_test.go
+++ organizationuser_test.go
@@ -190,35 +190,6 @@ func TestOrganizationUserListProjects(t *testing.T) {
 	}
 }
 
-func TestOrganizationUserListRolesWithOptionalParams(t *testing.T) {
-	t.Skip("Mock server tests are disabled")
-	baseURL := "http://localhost:4010"
-	if envURL, ok := os.LookupEnv("TEST_API_BASE_URL"); ok {
-		baseURL = envURL
-	}
-	if !testutil.CheckTestServer(t, baseURL) {
-		return
-	}
-	client := llamacloudadmin.NewClient(
-		option.WithBaseURL(baseURL),
-		option.WithAPIKey("My API Key"),
-	)
-	_, err := client.Organizations.Users.ListRoles(
-		context.TODO(),
-		"182bd5e5-6e1a-4fe4-a799-aa6d9a6ab26e",
-		llamacloudadmin.OrganizationUserListRolesParams{
-			ProjectID: llamacloudadmin.String("182bd5e5-6e1a-4fe4-a799-aa6d9a6ab26e"),
-		},
-	)
-	if err != nil {
-		var apierr *llamacloudadmin.Error
-		if errors.As(err, &apierr) {
-			t.Log(string(apierr.DumpRequest(true)))
-		}
-		t.Fatalf("err should be nil: %s", err.Error())
-	}
-}
-
 func TestOrganizationUserRemoveFromProject(t *testing.T) {
 	t.Skip("Mock server tests are disabled")
 	baseURL := "http://localhost:4010"
